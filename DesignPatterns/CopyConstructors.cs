72a49e99a69bb0fa21bbe0d957b1159056d5e761
Gheorghe Balan <GHBA@simcorp.com>
2021-04-27 10:41:05 +0200

    design patterns - added object copy

---
+++ DesignPatterns/CopyConstructors.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+    public class Address :IPrototype<Address>
+    {
+        public string StreetAddress, City, Country;
+
+        public Address(string streetAddress, string city, string country)
+        {
+            StreetAddress = streetAddress ?? throw new ArgumentNullException(paramName: nameof(streetAddress));
+            City = city ?? throw new ArgumentNullException(paramName: nameof(city));
+            Country = country ?? throw new ArgumentNullException(paramName: nameof(country));
+        }
+
+        public Address(Address other)
+        {
+            StreetAddress = other.StreetAddress;
+            City = other.City;
+            Country = other.Country;
+        }
+
+        public Address DeepCopy()
+        {
+           return new Address(StreetAddress, City, Country);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(City)}: {City}, {nameof(Country)}: {Country}";
+        }
+    }
+
+
+    public class Employee : IPrototype<Employee>
+    {
+        public string Name;
+        public Address Address;
+
+        public Employee(string name, Address address)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Address = address ?? throw new ArgumentNullException(paramName: nameof(address));
+        }
+
+        public Employee(Employee other)
+        {
+            Name = other.Name;
+            Address = new Address(other.Address);
+        }
+
+        public Employee DeepCopy()
+        {
+            return new Employee(Name, Address.DeepCopy());
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    public class CopyConstructors
+    {
+        static void Main(string[] args)
+        {
+            var john = new Employee("John", new Address("123 London Road", "London", "UK"));
+
+            //var chris = john;
+            var chris = john.DeepCopy();
+
+            chris.Name = "Chris";
+            Console.WriteLine(john); // oops, john is called chris
+            Console.WriteLine(chris);
+
+
+        }
+    }
+}
---
+++ DesignPatterns/CopyThroughPrototypeInheritance.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class CopyThroughPrototypeInheritance
+    {
+        public interface IDeepCopyable<T> where T : new()
+        {
+            void CopyTo(T target);
+
+            public T DeepCopy()
+            {
+                T t = new T();
+                CopyTo(t);
+                return t;
+            }
+        }
+        public class Address : IDeepCopyable<Address>
+        {
+            public string StreetName;
+            public int HouseNumber;
+
+            public Address()
+            {
+                
+            }
+            public Address(string streetName, int houseNumber)
+            {
+                StreetName = streetName;
+                HouseNumber = houseNumber;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+            }
+
+            public void CopyTo(Address target)
+            {
+                target.StreetName = StreetName;
+                target.HouseNumber = HouseNumber;
+            }
+        }
+
+        public class Person : IDeepCopyable<Person>
+        {
+            public string[] Names;
+            public Address Address;
+
+            public Person()
+            {
+                
+            }
+            public Person(string[] names, Address address)
+            {
+                Names = names;
+                Address = address;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+            }
+
+            public void CopyTo(Person target)
+            {
+                target.Names = (string[]) Names.Clone();
+                //target.Address = Address.DeepCopy();
+            }
+        }
+
+        public class Employee : Person, IDeepCopyable<Employee>
+        {
+            public int Salary;
+
+            public Employee()
+            {
+                
+            }
+
+            public Employee(int salary)
+            {
+                Salary = salary;
+            }
+            public override string ToString()
+            {
+                return $"{base.ToString()}, {nameof(Salary)}: {Salary}";
+            }
+
+            public void CopyTo(Employee target)
+            {
+                base.CopyTo(target);
+                target.Salary = Salary;
+            }
+        }
+    }
+}
